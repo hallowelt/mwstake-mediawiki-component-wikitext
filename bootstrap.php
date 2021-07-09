f9b11f8ff5934eeb6e8201d7d8c3a35381341401
pwirth <wirth@hallowelt.biz>
2021-07-09 12:11:26 +0200

    Initialize repo

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
+	return;
+}
+
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '1.0.0' );
+
+$GLOBALS['mwsgMenuParserRegistry'] = [];
