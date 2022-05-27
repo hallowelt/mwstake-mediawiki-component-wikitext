cca75c526e454ea340b1740a5fcfdbd4e98b5969
rvogel <vogel@hallowelt.biz>
2022-05-27 11:29:40 +0200

    Bump version number

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '3.0.1' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '3.0.2' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'wikitext', function () {
