68f91b9ef7fc24712f0c136ba384c0e860e72a3a
d.savuljesku <d.savuljesku@gmail.com>
2022-11-07 15:28:27 +0100

    Remove uncessary comment

--- bootstrap.php
+++ bootstrap.php
@@ -7,12 +7,10 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '5.0.2' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '5.0.3' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'wikitext', function () {
-		// TODO: Parsoid is not included in the requirements becuase its already required by MW core
-		// and so in a very specific version, so requiring it again might lead to collisions
 		wfLoadExtension( 'Parsoid', $GLOBALS['IP'] . '/vendor/wikimedia/parsoid/extension.json' );
 
 		$GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/ServiceWiring.php';
