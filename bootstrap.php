90b97780c6cfa548c1310c8f4b4d15ec6da39699
Hamish Slater <hamishslater@hotmail.com>
2022-11-09 16:37:19 +0100

    Remove Parsoid dependency

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '5.0.3' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '5.0.4' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'wikitext', function () {
