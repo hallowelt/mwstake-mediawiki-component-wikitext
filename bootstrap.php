a6e1d9e3782b0f6c49aef280e70ee7e9e1951000
Hamish Slater <hamishslater@hotmail.com>
2022-12-13 12:27:04 +0100

    Revert Remove Parsoid dependency

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '5.0.4' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '5.0.3' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'wikitext', function () {
