2ff0834d690c67b9782ca09b79d2ea8d98eb81ba
Hamish Slater <hamishslater@hotmail.com>
2023-01-18 16:47:48 +0100

    Revert version

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '6.0.1' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '6.0.0' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'wikitext', function () {
