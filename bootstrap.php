b5e23e774788ace18064cc630279493ead6ade2a
Hamish <hamish.slater@hallowelt.com>
2026-09-17 02:15:08 +0700

    Bump version

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '7.0.3' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_WIKITEXT_VERSION', '7.0.4' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 	->register( 'wikitext', static function () {
